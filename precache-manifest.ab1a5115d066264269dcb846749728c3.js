106562424998ec2810d6b55b5cb588608375633f
thakurnitin2684 <thakurnitin2684@gmail.com>
2022-06-11 21:35:55 +0530

    Updates

---
+++ precache-manifest.ab1a5115d066264269dcb846749728c3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a715c869e4bc516322a9a90c4b567afe",
+    "url": "/react-clock/index.html"
+  },
+  {
+    "revision": "846d5aa2480e8fe04c84",
+    "url": "/react-clock/static/js/2.7cfd8ec1.chunk.js"
+  },
+  {
+    "revision": "eb0b11edee8a16dd7006",
+    "url": "/react-clock/static/js/main.a71deecb.chunk.js"
+  },
+  {
+    "revision": "b5e0c384160469fa26fa",
+    "url": "/react-clock/static/js/runtime-main.59bf7f81.js"
+  },
+  {
+    "revision": "b44c6f8ea0dd9c804707efc44c61465c",
+    "url": "/react-clock/static/media/i1.b44c6f8e.jpeg"
+  },
+  {
+    "revision": "9928737a3504c5fc8269377d8ba5a122",
+    "url": "/react-clock/static/media/i2.9928737a.jpg"
+  },
+  {
+    "revision": "f8602e4aa2c1fb0cba44037b917737e5",
+    "url": "/react-clock/static/media/i3.f8602e4a.jpg"
+  }
+]);
